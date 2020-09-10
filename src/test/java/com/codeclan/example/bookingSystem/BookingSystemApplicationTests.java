d2b565608625457921afe4163e3d31e94c5af818
Alina <lindenalina.al@gmail.com>
2020-09-10 10:26:02 +0100

    initial commit

---
+++ src/test/java/com/codeclan/example/bookingSystem/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.bookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
