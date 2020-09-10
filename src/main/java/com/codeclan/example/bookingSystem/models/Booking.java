f999c3289af96e20b073439564b4ce78a8b83bb4
Alina <lindenalina.al@gmail.com>
2020-09-10 10:34:43 +0100

    course model set up

---
+++ src/main/java/com/codeclan/example/bookingSystem/models/Booking.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.bookingSystem.models;
+
+public class Booking {
+}
---
+++ src/main/java/com/codeclan/example/bookingSystem/models/Course.java
@@ -0,0 +1,81 @@
+package com.codeclan.example.bookingSystem.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "town")
+    private String town;
+
+    @Column(name = "starRating")
+    private int starRating;
+
+    @JsonIgnoreProperties({"course"})
+    @OneToMany(mappedBy = "course")
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE)
+    private List<Booking> bookings;
+
+    public Course(String name, String town, int starRating) {
+        this.name = name;
+        this.town = town;
+        this.starRating = starRating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
