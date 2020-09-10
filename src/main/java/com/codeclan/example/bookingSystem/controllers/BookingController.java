73b5ac69b20b1460a63c403a6fd40b2d0a684a06
Alina <lindenalina.al@gmail.com>
2020-09-10 11:28:50 +0100

    booking controller set up

--- src/main/java/com/codeclan/example/bookingSystem/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/bookingSystem/controllers/BookingController.java
@@ -1,4 +1,45 @@
 package com.codeclan.example.bookingSystem.controllers;
 
+import com.codeclan.example.bookingSystem.models.Booking;
+import com.codeclan.example.bookingSystem.repositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class BookingController {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @GetMapping(value = "/bookings")
+    public ResponseEntity<List<Booking>> getAllBookings() {
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/bookings/{id}")
+    public ResponseEntity getBooking(@PathVariable Long id) {
+        return new ResponseEntity(bookingRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/bookings")
+    public ResponseEntity<Booking> postBooking(@RequestBody Booking booking) {
+        bookingRepository.save(booking);
+        return new ResponseEntity<>(booking, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/bookings/{id}")
+    public ResponseEntity<Booking> putBooking(@RequestBody Booking booking) {
+        bookingRepository.save(booking);
+        return new ResponseEntity<>(booking, HttpStatus.OK);
+    }
+    
+    @DeleteMapping(value = "/bookings/{id}")
+    public ResponseEntity<List<Booking>> deleteBooking(@PathVariable Long id) {
+        bookingRepository.deleteById(id);
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
 }
