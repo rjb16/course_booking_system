6ed4c32d71cd6a66049630af40182746bd7eaa83
Alina <lindenalina.al@gmail.com>
2020-09-10 11:51:59 +0100

    Get all courses with a given rating

--- src/main/java/com/codeclan/example/bookingSystem/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/bookingSystem/controllers/CourseController.java
@@ -16,7 +16,12 @@ public class CourseController {
     CourseRepository courseRepository;
 
     @GetMapping(value = "/courses")
-    public ResponseEntity<List<Course>> getAllCourses() {
+    public ResponseEntity<List<Course>> getAllCourses(
+            @RequestParam(name="starRating", required = false) Integer starRating) {
+
+        if (starRating != null) {
+            return new ResponseEntity<>(courseRepository.findByStarRating(starRating), HttpStatus.OK);
+        }
         return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
     }
 
